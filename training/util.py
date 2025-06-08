48e5e1043639a3d2e87c284e3c1446830bf1a155
chloekoee <chloe.koe@gmail.com>
2025-06-08 17:57:25 +1000

    Clean up - separate out utility functions into util file

--- training/util.py
+++ training/util.py
@@ -1,5 +1,24 @@
 import numpy as np
 import os
+import torch
+from midvoxio.voxio import vox_to_arr
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+
+# ════════════════════════════════════════════════════════════════════
+#                           INPUT OUTPUT
+# ════════════════════════════════════════════════════════════════════
+
+
+def load_image(imagePath: str):
+    voxel = vox_to_arr(imagePath)
+    voxel_tensor = torch.tensor(voxel).float()
+    if torch.cuda.is_available():
+        voxel_tensor = voxel_tensor.to(torch.float64)
+    else:
+        voxel_tensor = voxel_tensor.to(torch.float32)
+
+    return voxel_tensor.permute(3, 0, 1, 2)
 
 
 def save_tensor(simulation_tensor, filenameBase="test", directory="tensors"):
@@ -57,3 +76,129 @@ def save_model_state(model, seed, filenameBase="test", directory="state"):
     state_dict["seed"] = seed
 
     np.save(f"{directory}/{filenameBase}", state_dict)
+
+
+# ════════════════════════════════════════════════════════════════════
+#                           TRAINING UTIL
+# ════════════════════════════════════════════════════════════════════
+
+
+def getLosses(fileName="losses.csv"):
+    losses = []
+
+    if not os.path.exists(fileName):
+        print("No existing losses detected")
+        return []
+    try:
+        with open(fileName, mode="r") as file:
+            while True:
+                buffer = file.read(8)  # Load float into buffer
+                if not buffer:  # exit if EOF
+                    break
+
+                file.read(1)  # skip the comma
+                losses.append(float(buffer))
+    except ValueError:
+        print("Losses file corrupted")
+        return []
+    return losses
+
+
+def initialiseGPU(model):
+    ## Check if GPU available
+    if torch.cuda.is_available():
+        print(f"GPU: {torch.cuda.get_device_name(0)} is available.")
+
+    ## Configure device as GPU
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(device)
+    model = model.to(device)
+    return model
+
+
+# ════════════════════════════════════════════════════════════════════
+#                           TENSOR OPERATIONS
+# ════════════════════════════════════════════════════════════════════
+
+
+def minimise_voxel(imgTensor):
+    imgTensor
+    non_zeros = (torch.nonzero(imgTensor[3, :, :, :])).permute(1, 0)
+
+    min_bounds = [torch.min(non_zeros[i]) for i in range(non_zeros.shape[0])]
+    max_bounds = [torch.max(non_zeros[i]) for i in range(non_zeros.shape[0])]
+
+    imgTensor = imgTensor[
+        :,
+        min_bounds[0] : max_bounds[0] + 1,
+        min_bounds[1] : max_bounds[1] + 1,
+        min_bounds[2] : max_bounds[2] + 1,
+    ]
+    return imgTensor
+
+
+def new_seed(target_voxel, channels=16, batch_size=1):
+    """
+    Seed is a cube map that sets a singular pixel activated in form
+    """
+    SHAPE = [target_voxel.shape[i] for i in range(len(target_voxel.shape))]
+    seed = torch.zeros(batch_size, channels, SHAPE[1], SHAPE[2], SHAPE[3])
+
+    ## Batch, channels, x, y, z
+    seed[:, 3, SHAPE[1] // 2, SHAPE[2] // 2, 0] = 1
+    return seed
+
+
+# ════════════════════════════════════════════════════════════════════
+#                           VISUALISATION
+# ════════════════════════════════════════════════════════════════════
+
+
+def visualise(imgTensor, filenameBase, save=True, show=False):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+
+    ## If imgTensor does not have batch dimension, add batch dimension of size 1
+    if imgTensor.ndim < 5:
+        imgTensor = imgTensor.unsqueeze(0)
+
+    ## Permute the tensor to (batch, x, y, z, channel) from (batch, channel, x, y, z)
+    imgTensor = imgTensor.permute(0, 2, 3, 4, 1)
+
+    ## Convert tensor to numpy array (as otherwise matplotlib cannot transpose it (when moveaxis is used))
+    imgTensor = imgTensor.detach().numpy()
+
+    ## Voxels look like they have their x and y swapped when plotted with matplotlib, so swap them for visualisation
+    imgTensor = np.moveaxis(imgTensor, (1, 2), (1, 2))
+
+    ## Calculate minimum edge length to ensure all voxels are cuboid
+    ax.set_box_aspect(imgTensor.shape[1:4] / np.max(imgTensor.shape[1:4]))
+
+    def update(imgIdx):
+        ## Clear axis to avoid overlaying plots
+        ax.cla()
+
+        ## Fix all axis range to show growth better
+        ax.set_xlim([0, imgTensor.shape[1]])
+        ax.set_ylim([0, imgTensor.shape[2]])
+        ax.set_zlim([0, imgTensor.shape[3]])
+
+        ## Only plot voxels with alpha channel > 0.1, and clip the RGBA channels to be between 0 and 1
+        ax.voxels(
+            filled=(imgTensor[imgIdx, :, :, :, 3] > 0.1),
+            facecolors=np.clip(imgTensor[imgIdx, :, :, :, :4], 0, 1),
+        )
+        ax.set_title(f"Frame {imgIdx}")
+
+    if save:
+        ## Create an animation with the number of frames equal to the time dimension
+        ani = animation.FuncAnimation(fig, update, frames=len(imgTensor), repeat=False)
+        writer = animation.PillowWriter(fps=5, metadata=dict(artist="Me"), bitrate=1800)
+        ani.save(filenameBase + ".gif", writer=writer)
+
+    if show:
+        update(imgIdx=0)
+        plt.show()
+        plt.close()
+
+    return
