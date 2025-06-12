Fix bug with new seed calculation

training/train.py
@@ -22,7 +22,7 @@ from util import (
 
 """
 target_voxel : rgba, x, y, z
-seed: rgba, x, y, z
+seed: batch, rgba, x, y, z
 output: batch, rgba, x, y, z
 """
 
@@ -191,10 +191,10 @@ if __name__ == "__main__":
     target_voxel = minimise_voxel(target_voxel).cpu()
     # anim = visualise(target_voxel, save=False, show=True)
 
-    # if os.path.exists(f"{VOXEL_PATH_NAME}.pth"):
-    #     MODEL.load_state_dict(
-    #         torch.load(f"{VOXEL_PATH_NAME}.pth", map_location=torch.device("cpu"))
-    #     )
+    if os.path.exists(f"saved_models/{VOXEL_PATH_NAME}.pth"):
+        MODEL.load_state_dict(
+            torch.load(f"saved_models/{VOXEL_PATH_NAME}.pth", map_location=torch.device("cpu"))
+        )
 
     if TRAINING:
         # losses = getLosses()
@@ -205,7 +205,7 @@ if __name__ == "__main__":
             DEBUG_MODE=DEBUG_MODE,
             # training_losses=None,
         )
-        torch.save(MODEL.state_dict(), f"{VOXEL_PATH_NAME}.pth")
+        torch.save(MODEL.state_dict(), f"saved_models/{VOXEL_PATH_NAME}.pth")
 
     # ## Switch state to evaluation to disable dropout e.g.
     MODEL.eval()

training/util.py
@@ -38,7 +38,7 @@ def save_tensor(simulation_tensor, filenameBase="test", directory="tensors"):
     ## Convert tensor to numpy array (as otherwise matplotlib cannot transpose it (when moveaxis is used))
     simulation_tensor = simulation_tensor.detach().numpy()
 
-    # ## Voxels look like they have their x and y swapped when plotted with matplotlib, so swap them for visualisation
+    # ## Voxels look like they have their y and z swapped when plotted with matplotlib, so swap them for visualisation
     simulation_tensor = np.moveaxis(simulation_tensor, (2, 3), (3, 2))
 
     ## Set alpha values < 0.1 to 0
@@ -145,24 +145,28 @@ def new_seed(target_voxel, channels=16, batch_size=1):
     seed = torch.zeros(batch_size, channels, SHAPE[1], SHAPE[2], SHAPE[3])
 
     x, y, z = compute_seed_position(target_voxel, SHAPE)
-    seed[:, x, y, z, 3] = 1
+    seed[:, 3, x, y, z] = 1
     return seed
 
 
 def compute_seed_position(target_voxel, shape):
-    x_ideal_index = shape[1] // 2
-    y_ideal_index = 0
-    z_ideal_index = shape[2] // 2
-    alive_cells_positions = np.where(target_voxel[..., 3] > 0)
+    # note that y and z are swapped around in target_voxel, thus height = z
+    c, x, y, z = shape
+    x_ideal_index = x // 2
+    y_ideal_index = y // 2
+    z_ideal_index = 0
+    alive_cells_positions = np.where(target_voxel[3, ..., 0] > 0) ## positions of alive cells on bottom row 
 
-    x_index = (np.abs(alive_cells_positions[0] - x_ideal_index)).argmin()
-    y_index = (np.abs(alive_cells_positions[1] - y_ideal_index)).argmin()
-    z_index = (np.abs(alive_cells_positions[2] - z_ideal_index)).argmin()
+    ## TODO: use argmin to figure out which index in alive_cell_positions
+    index = (np.abs(alive_cells_positions[0] - x_ideal_index) + np.abs(alive_cells_positions[1] - y_ideal_index)).argmin()
+
+    ## find the index in which we minimise the x and y dimensions whilst keep z = 0 (if none exist when z = 0 this 
+    ## should be cropped away by minimal cropping)
 
     return (
-        alive_cells_positions[0][x_index],
-        alive_cells_positions[1][y_index],
-        alive_cells_positions[2][z_index],
+        alive_cells_positions[0][index],
+        alive_cells_positions[1][index],
+        0
     )
 
 
@@ -222,7 +226,7 @@ def visualise(imgTensor, filenameBase, save=True, show=False):
         ## Create an animation with the number of frames equal to the time dimension
         ani = animation.FuncAnimation(fig, update, frames=len(imgTensor), repeat=False)
         writer = animation.PillowWriter(fps=5, metadata=dict(artist="Me"), bitrate=1800)
-        ani.save(filenameBase + ".gif", writer=writer)
+        ani.save("gif/"+filenameBase + ".gif", writer=writer)
 
     if show:
         update(imgIdx=0)